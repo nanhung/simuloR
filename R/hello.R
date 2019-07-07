86b481d4c3ea28e9d4e1c90f7c83f049b950441c
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-07-06 21:04:55 -0500

    add makemcsim

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,42 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+makemcsim <- function(model, deSolve = F, dir = "modeling"){
+
+  pkgwd <- find.package("simuloR")
+
+  exe_file <- paste0("mcsim.", model)
+  #if(file.exists(exe_file)) stop(paste0("* '", exe_file, "' had been created."))
+
+  if (deSolve == T){
+    system(paste("./MCSim/mod.exe -R ", dir, "/", model, " ", model, ".c", sep = ""))
+    system (paste0("R CMD SHLIB ", model, ".c")) # create *.dll files
+    dyn.load(paste(model, .Platform$dynlib.ext, sep="")) # load *.dll
+    source(paste0(model,"_inits.R"))
+  } else {
+
+    if(.Platform$OS.type == "windows"){
+      mod <- "mod.dll"
+      mod.wd <- paste0(pkgwd, "/libs/x64")
+    } else {
+      mod <- "mod.so"
+      mod.wd <- paste0(pkgwd, "/libs")
+    }
+
+    current.wd <- getwd()
+    setwd(mod.wd)
+    system(paste0("./", mod," ", current.wd, "/", model, " ", current.wd, "/", model, ".c"))
+    setwd(current.wd)
+
+    #system(paste("./MCSim/mod.exe ", model, " ", model, ".c", sep = ""))
+
+    #system(paste("gcc -O3 -I.. -I./MCSim/sim -o mcsim.", model, model, ".c ./MCSim/sim/*.c -lm ", sep = ""))
+    #invisible(file.remove(paste0(model, ".c")))
+    #if(file.exists(exe_file)) message(paste0("* Created executable program '", exe_file, "'."))
+  }
+}
+
+system("./mod.dll")
+
+
