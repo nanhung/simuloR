48c1a159fa41ba076902eff7ba55b01138031b56
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-08 10:16:54 -0600

    makemcsim + deSolve

--- R/hello.R
+++ R/hello.R
@@ -19,7 +19,6 @@ hello <- function() {
 
 makemcsim <- function(file, deSolve = F, dir = "modeling"){
 
-  current.wd <- getwd()
   dir.pkg <- find.package("simuloR")
   dir.file <- dirname(file)
   dir.sim <- system.file('sim', package = 'simuloR')
@@ -27,32 +26,31 @@ makemcsim <- function(file, deSolve = F, dir = "modeling"){
   mStr <- strsplit(file, "/")
   mName <- mStr[[1]][length(mStr[[1]])]
   exe_file <- paste0(dir.file, "/mcsim.", mName)
-  #if(file.exists(exe_file)) stop(paste0("* '", exe_file, "' had been created."))
 
-  if (deSolve == T){
-    system(paste("./MCSim/mod.exe -R ", dir, "/", mName, " ", mName, ".c", sep = ""))
-    system (paste0("R CMD SHLIB ", mName, ".c")) # create *.dll files
-    dyn.load(paste(mName, .Platform$dynlib.ext, sep="")) # load *.dll
-    source(paste0(mName,"_inits.R"))
+  if(.Platform$OS.type == "windows"){
+    mod <- "mod.dll"
+    dir.mod <- paste0(dir.pkg, "/libs/x64")
   } else {
-
-    if(.Platform$OS.type == "windows"){
-      mod <- "mod.dll"
-      dir.mod <- paste0(dir.pkg, "/libs/x64")
-    } else {
-      mod <- "mod.so"
-      dir.mod <- paste0(dir.pkg, "/libs")
-    }
-
-    invisible(file.copy(from = paste0(dir.mod, "/",mod), to = paste0(getwd(), "/",mod)))
-    system(paste0("./", mod," ", file, " ", dir.file, "/",mName, ".c"))
-    invisible(file.remove(paste0(getwd(), "/",mod)))
-
-    message(paste0("* Creating executable program, pleas wait..."))
-    system(paste("gcc -O3 -I.. -I.", dir.sim, " -o ", dir.file, "/mcsim.", mName, " ", dir.file, "/", mName, ".c ", dir.sim, "/*.c -lm ", sep = ""))
-    invisible(file.remove(paste0(dir.file, "/", mName, ".c")))
-    if(file.exists(exe_file)) message(paste0("* Created executable program '", exe_file, "'."))
+    mod <- "mod.so"
+    dir.mod <- paste0(dir.pkg, "/libs")
   }
+  invisible(file.copy(from = paste0(dir.mod, "/",mod), to = paste0(getwd(), "/",mod)))
+
+  invisible(system(paste0("./", mod," -R ", file, " ", dir.file, "/",mName, ".c"), intern = T))
+  system(paste0("R CMD SHLIB ", dir.file, "/", mName, ".c "))
+  dyn.load(paste(dir.file, "/", mName, .Platform$dynlib.ext, sep=""))
+  source(paste0(dir.file, "/", mName, "_inits.R"))
+
+  system(paste0("./", mod," ", file, " ", dir.file, "/",mName, ".c"))
+  message(paste0("* Creating executable program, pleas wait..."))
+  system(paste("gcc -O3 -I.. -I.", dir.sim, " -o ", dir.file, "/mcsim.", mName, " ", dir.file, "/", mName, ".c ", dir.sim, "/*.c -lm ", sep = ""))
+
+  invisible(file.remove(paste0(getwd(), "/",mod)))
+  invisible(file.remove(paste0(dir.file, "/", mName, ".c")))
+  invisible(file.remove(paste(dir.file, "/", mName, .Platform$dynlib.ext, sep="")))
+  invisible(file.remove(paste0(dir.file, "/", mName, "_inits.R")))
+
+  if(file.exists(exe_file)) message(paste0("* Created executable program '", exe_file, "'."))
 }
 
 mods <- function(){
