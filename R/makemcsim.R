91ecaf9d127a2ac37f09bd4014f2868ad7b52877
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-22 14:29:29 -0600

    test in RStudio Cloud

--- R/makemcsim.R
+++ R/makemcsim.R
@@ -31,7 +31,8 @@ makemcsim <- function(file, init = F){
 
   dir.pkg <- find.package("simuloR")
   dir.file <- dirname(file)
-  dir.sim <- system.file('sim', package = 'simuloR')
+  #dir.sim <- system.file('sim', package = 'simuloR')
+  dir.sim <- paste0(getwd(), "/mcsim-6.1.0/sim")
 
   mStr <- strsplit(file, "/")
   mName <- mStr[[1]][length(mStr[[1]])]
@@ -63,6 +64,7 @@ makemcsim <- function(file, init = F){
 
   system(paste0("./", mod," ", file, " ", dir.file, "/",mName, ".c"))
   message(paste0("* Creating executable program, pleas wait..."))
+  #system(paste("gcc -O3 -I.. -I.", dir.sim, " -o ", dir.file, "/mcsim.", mName, " ", dir.file, "/", mName, ".c ", dir.sim, "/*.c -lm ", sep = ""))
   system(paste("gcc -O3 -I.. -I.", dir.sim, " -o ", dir.file, "/mcsim.", mName, " ", dir.file, "/", mName, ".c ", dir.sim, "/*.c -lm ", sep = ""))
   if(file.exists(exe_file)) message(paste0("* Created executable program '", exe_file, "'."))
 
