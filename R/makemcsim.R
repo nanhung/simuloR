1069a6591eed49bfeb9fabc08ebbf3db9e998990
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-22 14:15:45 -0600

    download

--- R/makemcsim.R
+++ R/makemcsim.R
@@ -73,28 +73,3 @@ makemcsim <- function(file, init = F){
 
   if (exists("x"))  return(x)
 }
-
-#' @export
-mcsim_download <- function() {
-
-  version<-"6.1.0"
-  URL <- sprintf('http://ftp.gnu.org/gnu/mcsim/mcsim-%s.tar.gz', version)
-  tf <- tempfile()
-
-
-  message(paste0("Downloading GNU MCSim " , sprintf('%s', version), " ..."))
-  download.file(URL, tf, mode = "wb")
-
-  utils::untar(tf, exdir = getwd())
-
-  file <- paste0(getwd(), "/mcsim-6.1.0/sim/lsodes1.c")
-  lsodes1.c <- readLines(file)
-  new.mxstp0 <- paste0("mxstp0 = ", 5000)
-  mxstp0 <- gsub("mxstp0 = 500", new.mxstp0, lsodes1.c)
-  cat(mxstp0, file=file, sep="\n")
-
-  cat("\n")
-  if (file.exists("mcsim-6.1.0")){
-    message(paste0("The GNU MCSim " , sprintf('%s', version), " has been successfully downloaded."))
-  }
-}
