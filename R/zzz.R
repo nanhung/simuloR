1647e302df7790baa3bf30ed29ba53d6af1300bc
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-12-12 21:45:47 -0600

    update

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,9 @@
-if (Sys.info()[['sysname']] == "Windows") {
-  if(Sys.which("gcc") == ""){ # echo $PATH
-    PATH = "c:/Rtools/mingw_32/bin"
-    Sys.setenv(PATH = paste(PATH, Sys.getenv("PATH"), sep=";"))
-  } # PATH=$PATH:/c/Rtools/mingw_32/bin; export PATH
-} # PATH=$PATH:/c/MinGW/msys/1.0/local/bin
-
 .onAttach <- function(...){
   packageStartupMessage("The package is powered by GNU MCSim v6.1.0.")
+  if (Sys.info()[['sysname']] == "Windows") {
+    if(Sys.which("gcc") == ""){ # echo $PATH
+      PATH = "c:/Rtools/mingw_32/bin"
+      Sys.setenv(PATH = paste(PATH, Sys.getenv("PATH"), sep=";"))
+    } # PATH=$PATH:/c/Rtools/mingw_32/bin; export PATH
+  } # PATH=$PATH:/c/MinGW/msys/1.0/local/bin
 }
